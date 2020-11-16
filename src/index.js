8609674d88a6f4e6988a4b3b745fe2b783d4f654
SylwiaOlejko <63730148+SylwiaOlejko@users.noreply.github.com>
2020-11-16 18:36:35 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let country = "Guinea Bissau";
+console.log(country.replace(" ","-"));
+
+let city = " Sydney   ";
+city = city.trim();
+console.log(city.trim());
+
+let place = "School";
+console.log(place.toUpperCase());
+
+let attraction = "Opera House";
+console.log(attraction.toLowerCase());
+
+let sentence = "I went to visit the " +  attraction + "in " + city + ", right next to my " + place + ". Now, I'm getting ready for my trip to " + country + "!";
+
+console.log(sentence);
